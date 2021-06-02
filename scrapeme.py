6b05e2133ff859277848c8d53194981a9557372c
MattH <mjhemming1@gmail.com>
2021-06-02 08:13:19 +0100

    comment added

--- scrapeme.py
+++ scrapeme.py
@@ -1,6 +1,8 @@
 from urllib import request
 from bs4 import BeautifulSoup
 
+# a comment
+
 url = 'http://bbc.co.uk'
 userAgent = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'}
 
